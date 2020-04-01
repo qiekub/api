a7278fa173c916b7345768b7dd59ca6ed2334b1b
Thomas Rosen <thomas.rosen@me.com>
2020-04-01 17:41:19 +0200

    added .env file for the secrets

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 // console.log('process.env', process.env.FUNCTIONS_EMULATOR)
 
--- functions/secretManager.js
+++ functions/secretManager.js
@@ -1,20 +1,23 @@
 const env = require('node-env-file')
-env(`${__dirname}/../.env`)
+env(`${__dirname}/.env`)
 
 // TODO
 // !!!!!! The following object should't be stored within this file or even exist! !!!!!!
 // It is here so all secrets are in one place and can be put somewhere else with ease.
 // Look into "rotating secrets"
-const secrets = {
-	mongodb_username: process.env.mongodb_username,
-	mongodb_password: process.env.mongodb_password,
-	mongodb_server_domain: process.env.mongodb_server_domain,
+//
+// const secrets = {
+// 	mongodb_username: process.env.mongodb_username,
+// 	mongodb_password: process.env.mongodb_password,
+// 	mongodb_server_domain: process.env.mongodb_server_domain,
+//
+// 	api_key_locationiq: process.env.api_key_locationiq,
+// 	api_key_opencagedata: process.env.api_key_opencagedata,
+// 	api_key_googleapis: process.env.api_key_googleapis,
+// 	api_key_mapquestapi: process.env.api_key_mapquestapi,
+// }
 
-	api_key_locationiq: process.env.api_key_locationiq,
-	api_key_opencagedata: process.env.api_key_opencagedata,
-	api_key_googleapis: process.env.api_key_googleapis,
-	api_key_mapquestapi: process.env.api_key_mapquestapi,
-}
+const secrets = process.env
 
 
 
