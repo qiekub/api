3881349c6a001fb77bfc3aa8406f01b2506acbac
Thomas Rosen <thomas.rosen@me.com>
2020-04-16 00:34:47 +0200

    BUGFIXES wrong filepaths and missing async

--- functions/modules/isGeoCoordinateLegalPromise.js
+++ functions/modules/isGeoCoordinateLegalPromise.js
@@ -1,9 +1,9 @@
-const secretManager = require('./secretManager.js')
+const secretManager = require('../secretManager.js')
 const getSecretAsync = secretManager.getSecretAsync
 
 const fetch = require('node-fetch')
 
-const legalCountries = require('./data/dist/legalCountries.json')
+const legalCountries = require('../data/dist/legalCountries.json')
 const countryCodes = legalCountries.countryCodes
 
 async function tryToFetchJson(url, mapping) {
@@ -25,8 +25,8 @@ async function tryToFetchJson(url, mapping) {
 	return new Promise(resolve => resolve(result)).then(mapping)
 }
 
-function isGeoCoordinateLegalPromise(lat,lng){
-	return tryToFetchJson(`https://api.mapbox.com/geocoding/v5/mapbox.places/${args.lng},${args.lat}.json?types=country&limit=1&access_token=${await getSecretAsync('api_key_mapbox')}`, data => {
+async function isGeoCoordinateLegalPromise(lat,lng){
+	return tryToFetchJson(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=country&limit=1&access_token=${await getSecretAsync('api_key_mapbox')}`, data => {
 		const features = data.features
 		if (features && Array.isArray(features) && features.length > 0) {
 			const short_code = features[0].properties.short_code
