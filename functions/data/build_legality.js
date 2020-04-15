d1c631b6aee1886a2f0cfa67470e8fe7462ece0f
Thomas Rosen <thomas.rosen@me.com>
2020-04-15 23:16:14 +0200

    Use a hard-coded country-name-synonyms-list instead of levenstein.

--- functions/data/build_legality.js
+++ functions/data/build_legality.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const levenshtein = require('js-levenshtein')
 
 /*
 const files = fs.readdirSync('./questions/')
@@ -48,62 +47,38 @@ fs.writeFileSync(distPath+'questionsInSchema.json', JSON.stringify(questionsInSc
 */
 
 
+const synonyms = {
+	// TODO: are these all correct?
+	'North Korea': "Korea (Democratic People's Republic of)",
+	'South Korea': "Korea (Republic of)",
+	'São Tome & Principe': "Sao Tome and Principe",
+	'North Macedonia': "Macedonia (the former Yugoslav Republic of)",
+	'Democratic Republic of Congo': "Congo",
+	'Cape Verde': "Cabo Verde",
+}
 
-
-let countryNames = new Set()
 
 const alpha2Codes = require('./laws_around_the_world/all_countries.json')
 .reduce((obj,entry) => {
 	obj[entry.name] = entry.alpha2Code
 	obj[entry.nativeName] = entry.alpha2Code
 	obj[entry.demonym] = entry.alpha2Code
-	
-	countryNames.add(entry.name)
-	countryNames.add(entry.nativeName)
-	// countryNames.add(entry.demonym)
 
 	for (const language in entry.translations) {
 		obj[entry.translations[language]] = entry.alpha2Code
-		countryNames.add(entry.translations[language])
 	}
 	for (const language in entry.altSpellings) {
 		obj[entry.altSpellings[language]] = entry.alpha2Code
-		countryNames.add(entry.altSpellings[language])
 	}
 
 	return obj
 }, {})
 
-countryNames = [...countryNames].filter(name => !!name)
-
-function getBestFittingName(countryName){
-	const tmp_countryNames = countryNames.map(name => {
-		return {
-			name: name,
-			countryName: countryName,
-			score: levenshtein(name, countryName),
-		}
-	})
-	// .filter(entry => entry.score < 5)
-	.sort((a,b) => a.score - b.score)
-
-	console.log(JSON.stringify(tmp_countryNames.slice(0,3),null,'\t'))
-
-	if (!!tmp_countryNames[0]) {
-		return tmp_countryNames[0].name
-	}
-
-	return countryName
-}
-
 function getCountryCode(countryName){
 	if (alpha2Codes[countryName]) {
 		return alpha2Codes[countryName]
-	}else{
-		countryName = getBestFittingName(countryName)
-		if (alpha2Codes[countryName]) {
-			return alpha2Codes[countryName]
-		}
+	} else if (synonyms[countryName] && alpha2Codes[synonyms[countryName]]) {
+		return alpha2Codes[synonyms[countryName]]
 	}
 
 	return countryName
@@ -125,9 +100,7 @@ fs.readFileSync('./laws_around_the_world/Same-Sex Sexual Acts Legality.csv', 'ut
 	return legalCountriesTmp
 }, [])
 
-legalCountries = {
-	legalCountries: [... new Set(legalCountries)],
-}
+legalCountries = [... new Set(legalCountries)].join(' ')
 // END get legalCountries
 
 
@@ -137,7 +110,7 @@ if (!fs.existsSync(distPath)){
 	fs.mkdirSync(distPath)
 }
 
-fs.writeFileSync(distPath+'legalCountries.json', JSON.stringify(legalCountries,null,'\t'))
+fs.writeFileSync(distPath+'legalCountries.text', legalCountries)
 
 
 // console.log(JSON.stringify(legalCountries, null, 4))
