6f812a1a076075b1860da75953d3600e7f71e45d
Thomas Rosen <thomas.rosen@me.com>
2020-04-23 08:20:18 +0200

    added getPreset function

--- functions/modules.js
+++ functions/modules.js
@@ -556,10 +556,42 @@ function compileAnswers(mongodb, placeIDs, callback){
 }
 
 
+function getPreset(tags, presets) {
+	const tags_keys = Object.keys(tags)
+	for (const preset_key in presets) {
+		const preset_tags = presets[preset_key].tags
+		const preset_tags_keys = Object.keys(preset_tags)
+
+		const common_keys = preset_tags_keys.filter(key => tags_keys.includes(key))
+
+		if (common_keys.length === preset_tags_keys.length) {
+			const okay_keys = common_keys.filter(key => preset_tags[key] === '*' || tags[key].includes(preset_tags[key]))
+			if (okay_keys.length === preset_tags_keys.length) {
+				return {
+					key: preset_key,
+					...presets[preset_key],
+				}
+			}
+		}
+	}
+
+
+	return {
+		"key": "",
+		"tags_length": 0,
+		"max_tag_value_length": 0,
+		"tags": {},
+		"name": {},
+		"terms": {}
+	}
+	// return presets[Object.keys(presets)[0]]
+}
+
 
 module.exports = {
 	addAnswer,
 	upsertOne,
 	compileAnswers,
 	isGeoCoordinateLegalPromise,
+	getPreset,
 }
