9d3a488d826cf0aa5837b7120339ae787b87735b
Thomas Rosen <thomas.rosen@me.com>
2020-04-26 09:00:30 +0200

    removed old collections

--- functions/getMongoDbContext.js
+++ functions/getMongoDbContext.js
@@ -25,11 +25,7 @@ function getMongoDbContext(){
 						client: mongodb_client,
 						ObjectID: ObjectID,
 	
-						// collection: mongodb_client.db('Graph').collection('CompiledPlaces'),
 						CompiledPlaces_collection: mongodb_client.db('Graph').collection('CompiledPlaces'),
-						OsmCache_collection: mongodb_client.db('Graph').collection('OsmCache'),
-						Answers_collection: mongodb_client.db('Graph').collection('Answers'),
-						Sources_collection: mongodb_client.db('Graph').collection('Sources'),
 						Changesets_collection: mongodb_client.db('Graph').collection('Changesets'),
 					}
 	
