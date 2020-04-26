fcecdefc99a7a92c0f5597e8cf73ab9f5584c205
Thomas Rosen <thomas.rosen@me.com>
2020-04-26 11:20:31 +0200

    removed comments

--- functions/index.js
+++ functions/index.js
@@ -7,12 +7,5 @@ if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'graphql') {
 }
 
 if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'loadChanges') {
-	// TODO this should be timed
 	exports.loadChanges = require('./osm/loadChanges.js')
-
-	// exports.loadOsmData = functions.https.onRequest((req, res) => {	
-	// 	loadOsmData(()=>{
-	// 		res.send(JSON.stringify(Object.keys(mongodb),null,4))
-	// 	})
-	// })
 }
