8abc85b13881aceca7100aab53d9ed43633615e3
Thomas Rosen <thomas.rosen@me.com>
2020-05-21 02:38:21 +0200

    removed unnecessary code

--- functions/graphql/resolvers/getQuestions.js
+++ functions/graphql/resolvers/getQuestions.js
@@ -1,9 +1,3 @@
-const async = require('async')
-
 const questionsInSchema = require('../../data/dist/questionsInSchema.json')
 
-module.exports = async (parent, args, context, info) => {
-	const mongodb = context.mongodb
-
-	return questionsInSchema
-}
+module.exports = () => questionsInSchema
