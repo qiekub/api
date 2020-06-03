ad987da7ce4d145668bfdb78c22ea265ab8fc19f
Thomas Rosen <thomas.rosen@me.com>
2020-06-03 10:21:58 +0200

    return the changesets that are not yet in CompiledPlaces if no forID is provided

--- functions/graphql/resolvers/changesets.js
+++ functions/graphql/resolvers/changesets.js
@@ -26,7 +26,28 @@ module.exports = async (parent, args, context, info) => {
 				}
 			})
 		}else{
-			reject('not valid forID')
+			// return the changesets that are not yet in CompiledPlaces if no forID is provided
+			mongodb.Changesets_collection.aggregate([
+				{$lookup:{
+					from: 'CompiledPlaces',
+					localField: 'properties.forID',
+					foreignField: '_id',
+					as: 'lookup_result',
+				}},
+				{$match: {
+					'lookup_result': {$size:0},
+				}},
+				{$sort: {
+					'metadata.lastModified': -1,
+				}},
+				{$limit: 10},
+			]).toArray((error,docs)=>{
+				if (error) {
+					reject(error)
+				}else{
+					resolve(docs)
+				}
+			})
 		}
 	})
 }
