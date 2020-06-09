18d8a17ad0dbb9af818a46849f7a70fba748b1b7
Thomas Rosen <thomas.rosen@me.com>
2020-06-09 12:05:06 +0200

    user needs to be loggedin to view changesets

--- functions/graphql/resolvers/getChangeset.js
+++ functions/graphql/resolvers/getChangeset.js
@@ -2,7 +2,10 @@ module.exports = async (parent, args, context, info) => {
 	const mongodb = context.mongodb
 	
 	return new Promise((resolve,reject)=>{
-		if (!args._id) {
+		if (!(!!context.profileID)) {
+			// check if logged-in
+			reject('User must be logged in.')
+		}else if (!args._id) {
 			reject('No _id value')
 		}else if (!mongodb.ObjectID.isValid(args._id)) {
 			reject('_id is not a correct ObjectID')
