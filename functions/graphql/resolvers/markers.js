5e77bda9cced0ffda5d69ba36c8d9684491c759a
Thomas Rosen <thomas.rosen@me.com>
2020-06-21 06:49:18 +0200

    use single quotes

--- functions/graphql/resolvers/markers.js
+++ functions/graphql/resolvers/markers.js
@@ -4,18 +4,18 @@ module.exports = async (parent, args, context, info) => {
 	return new Promise((resolve,reject)=>{
 		mongodb.CompiledPlaces_collection.aggregate([
 			{$match:{
-				"properties.tags.preset": {$not: {$in:["default","boundary/administrative"]}},
+				'properties.tags.preset': {$not: {$in:['default','boundary/administrative']}},
 			}},
 			{$project:{
-				_id: "$_id",
-				name: "$properties.name",
-				lng: "$properties.geometry.location.lng",
-				lat: "$properties.geometry.location.lat",
-				preset: "$properties.tags.preset",
+				_id: '$_id',
+				name: '$properties.name',
+				lng: '$properties.geometry.location.lng',
+				lat: '$properties.geometry.location.lat',
+				preset: '$properties.tags.preset',
 				tags: {
-					min_age: "$properties.tags.min_age",
-					max_age: "$properties.tags.max_age",
-					audience_queer: "$properties.tags.audience:queer",
+					min_age: '$properties.tags.min_age',
+					max_age: '$properties.tags.max_age',
+					audience_queer: '$properties.tags.audience:queer',
 				},
 			}},
 		]).toArray((error,docs)=>{
