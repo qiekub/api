9fc499da5f0392355f934b4c94de6192035a661c
Thomas Rosen <thomas.rosen@me.com>
2020-08-15 15:34:23 +0200

    whitespace

--- functions/graphql/schema.js
+++ functions/graphql/schema.js
@@ -17,6 +17,7 @@ const schema = gql`
 
 		search(query: String, languages: [String]): SearchInfo
 		countrycode(lat: Float, lng: Float): String
+		
 		placesWithUndecidedChangesets(forID: ID): [Doc]
 		undecidedChangesets(forID: ID): [Doc]
 
