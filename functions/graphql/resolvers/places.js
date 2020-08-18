a14d932da3241743fb4e23ead0a00eb4faa7401e
Thomas Rosen <thomas.rosen@me.com>
2020-08-18 15:15:04 +0200

    check if logged-in. And only then return unpublished places.

--- functions/graphql/resolvers/places.js
+++ functions/graphql/resolvers/places.js
@@ -2,7 +2,15 @@ module.exports = async (parent, args, context, info) => {
 	const mongodb = context.mongodb
 
 	return new Promise((resolve,reject) => {
-		mongodb.CompiledPlaces_collection.find().toArray((error,docs)=>{
+		mongodb.CompiledPlaces_collection.find({
+			...(
+				!(!!context.profileID) // check if logged-in
+				? {
+					'properties.tags.published': true,
+				}
+				: {}
+			),
+		}).toArray((error,docs)=>{
 			if (error) {
 				reject(error)
 			}else{
