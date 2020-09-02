4efb81892fa2a58715244900c494ffb0829dcbe2
Thomas Rosen <thomas.rosen@me.com>
2020-09-02 22:36:10 +0200

    capped UberspaceServer.js to the minimum code amount

--- functions/graphql/UberspaceServer.js
+++ functions/graphql/UberspaceServer.js
@@ -1,9 +1,16 @@
 const gqlServer = require('./server.js')
 const app = gqlServer()
-const port = 11692 // api2 in english alphabet letter positions
+const port = 11692 // "api2" in english alphabet letter positions
 const host = '0.0.0.0'
-const path = '/graphql/v1'
+const urlpath = '/graphql/v1'
+
+app.use((req, res, next) => {
+	res.append('Access-Control-Allow-Origin', ['*'])
+	res.append('Access-Control-Allow-Methods', '*')
+	res.append('Access-Control-Allow-Headers', '*')
+	next()
+})
 
 app.listen(port, host, () => {
-	console.log(`App listening at http://${host}:${port}${path}`)
+	console.log(`App listening at http://${host}:${port}${urlpath}`)
 })
