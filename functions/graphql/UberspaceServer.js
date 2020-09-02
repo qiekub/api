acc544bc026b5847735ad6a8880d597ac6def5f3
Thomas Rosen <thomas.rosen@me.com>
2020-09-02 02:33:26 +0200

    added files to run the function on uberspace

---
+++ functions/graphql/UberspaceServer.js
@@ -0,0 +1,9 @@
+const gqlServer = require('./server.js')
+const app = gqlServer()
+const port = 11692 // api2 in english alphabet letter positions
+const host = '0.0.0.0'
+const path = '/graphql/v1'
+
+app.listen(port, host, () => {
+  console.log(`App listening at http://${host}:${port}${path}`)
+})
